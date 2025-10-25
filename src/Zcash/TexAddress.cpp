d6b6d08257477ba295b82550ea11a121da957d95
xuelongqy <xuelongqy@qq.com>
2025-10-25 20:57:59 +0800

    feat: validate TexAddress using condition

--- src/Zcash/TexAddress.cpp
+++ src/Zcash/TexAddress.cpp
@@ -10,12 +10,28 @@ bool TexAddress::isValid(const std::string& addr) {
 }
 
 bool TexAddress::isValid(const std::string& addr, const std::string& hrp) {
-    try {
-        TexAddress address(addr);
-        return hrp.empty() || address.getHrp() == hrp;
-    } catch (...) {
+    auto dec = Bech32::decode(addr);
+    // Check if decoding failed
+    if (std::get<1>(dec).empty()) {
         return false;
     }
+    // Check if it's Bech32M encoding
+    if (std::get<2>(dec) != Bech32::Bech32M) {
+        return false;
+    }
+    // Check hrp if provided
+    if (!hrp.empty() && std::get<0>(dec) != hrp) {
+        return false;
+    }
+
+    // Convert from 5-bit to 8-bit encoding
+    Data conv;
+    auto success = Bech32::convertBits<5, 8, false>(conv, std::get<1>(dec));
+    if (!success || conv.size() != length) {
+        return false;
+    }
+
+    return true;
 }
 
 TexAddress::TexAddress(const std::string& addr) {
